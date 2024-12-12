49663f9a1944559a886d6856df32425a2624021e
Folarinde ALLAGBE <folarindeallagbe@MacBook-Air-de-Folarinde.local>
2024-12-12 19:10:23 +0100

    the commit

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -3,16 +3,17 @@ pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import {Counter} from "../src/Counter.sol";
+import {MyNFT} from "../src/MyNFT.sol";
 
 contract CounterScript is Script {
-    Counter public counter;
+    MyNFT public myNft;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        myNft = new MyNFT();
 
         vm.stopBroadcast();
     }
